cec64b2e7efe149f19b28d7f44a428617b175be4
shahintahsan225@gmail.com <shahin550@@##&&>
2023-02-18 11:55:11 +0600

    git attributes added

--- lib/details/details_screen.dart
+++ lib/details/details_screen.dart
@@ -39,76 +39,79 @@ class DetailsScreen extends StatelessWidget {
           ),
           const SizedBox(height: defaultPadding * 1.5),
           Expanded(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(defaultPadding,
-                  defaultPadding * 2, defaultPadding, defaultPadding),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(defaultBorderRadius * 3),
-                  topRight: Radius.circular(defaultBorderRadius * 3),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(defaultPadding,
+                    defaultPadding * 2, defaultPadding, defaultPadding),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(defaultBorderRadius * 3),
+                    topRight: Radius.circular(defaultBorderRadius * 3),
+                  ),
                 ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          product.title,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            product.title,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        ),
+                        const SizedBox(width: defaultPadding),
+                        Text(
+                          "\$" + product.price.toString(),
                           style: Theme.of(context).textTheme.headline6,
                         ),
-                      ),
-                      const SizedBox(width: defaultPadding),
-                      Text(
-                        "\$" + product.price.toString(),
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: defaultPadding),
-                    child: Text(
-                      "A Henley shirt is a collarless pullover shirt, by a round neckline and a placket about 3 to 5 inches (8 to 13 cm) long and usually having 2–5 buttons.",
+                      ],
                     ),
-                  ),
-                  Text(
-                    "Colors",
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                  const SizedBox(height: defaultPadding / 2),
-                  Row(
-                    children: const [
-                      ColorDot(
-                        color: Color(0xFFBEE8EA),
-                        isActive: false,
-                      ),
-                      ColorDot(
-                        color: Color(0xFF141B4A),
-                        isActive: true,
-                      ),
-                      ColorDot(
-                        color: Color(0xFFF4E5C3),
-                        isActive: false,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: defaultPadding * 2),
-                  Center(
-                    child: SizedBox(
-                      width: 200,
-                      height: 48,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                            primary: primaryColor,
-                            shape: const StadiumBorder()),
-                        child: const Text("Add to Cart"),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: defaultPadding),
+                      child: Text(
+                        "A Henley shirt is a collarless pullover shirt, by a round neckline and a placket about 3 to 5 inches (8 to 13 cm) long and usually having 2–5 buttons.",
                       ),
                     ),
-                  )
-                ],
+                    Text(
+                      "Colors",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                    const SizedBox(height: defaultPadding / 4),
+                    Row(
+                      children: const [
+                        ColorDot(
+                          color: Color(0xFFBEE8EA),
+                          isActive: false,
+                        ),
+                        ColorDot(
+                          color: Color(0xFF141B4A),
+                          isActive: true,
+                        ),
+                        ColorDot(
+                          color: Color(0xFFF4E5C3),
+                          isActive: false,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: defaultPadding * 1),
+                    Center(
+                      child: SizedBox(
+                        width: 200,
+                        height: 48,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              primary: primaryColor,
+                              shape: const StadiumBorder()),
+                          child: const Text("Add to Cart"),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           )
